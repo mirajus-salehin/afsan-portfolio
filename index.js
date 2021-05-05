60d4b1ba5f750b872dece3195a99ddacfd22d6e6
Mirajus Salehin <mirajussalehin@gmail.com>
2021-05-05 13:12:53 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("div.button");
+const menu = document.querySelector("div.mobile-menu");
+
+button.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
